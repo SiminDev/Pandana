113937cc6f33dd1b76469476065c8375f350c4d1
SiminDev <simin.rahmani94@gmail.com>
2025-02-12 21:51:25 +0330

    create product card UI

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import Slider from '../../components/Slider'
+import ProductItem from "../../components/productItem/ProductItem";
+import Slider from "../../components/Slider";
 
 function Home() {
   return (
     <>
-    <div className='flex px-10 py-10 bg-slate-100'>
-    <Slider />
-    </div>
+      <div className="flex px-10 py-10 bg-slate-100">
+        <Slider />
+      </div>
+      <section className="px-10 py-10">
+        <h2 className="text-2xl mb-8 font-light text-gray-600">Latest products</h2>
+      <div className=" grid grid-cols-4 gap-8">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
