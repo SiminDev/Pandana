e85ade9598b223e5ee583d014e4a67edcc0c58eb
SiminDev <simin.rahmani94@gmail.com>
2025-02-12 23:40:01 +0330

    change navigation item and ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,24 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className=" px-10 py-4 grid grid-cols-3 items-center">
+    <div className=" px-10 py-4 grid grid-cols-3 items-center border-b border-b-slate-200">
       <div className="">
         <img src='/assets/pandana-logo.svg' />
       </div>
       <nav className="flex">
         <ul className="flex gap-4">
           <li>
-            <Link to="/home">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/all-products">All Products</Link>
           </li>
-          <li>
-            <Link to="/clothing">Clothing</Link>
-          </li>
-          <li>
-            <Link to="/jewelry">Jewelry</Link>
-          </li>
+          
         </ul>
       </nav>
     </div>
