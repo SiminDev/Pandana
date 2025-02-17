88dba260118ba22d9c8ec0635275cebbf0ca29ba
SiminDev <simin.rahmani94@gmail.com>
2025-02-17 22:23:18 +0330

    some design changes in product page

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getProduct } from "../../services/api";
 import { Product as IProduct } from "../../types/server";
 import { useParams } from "react-router-dom";
+import { StarIcon } from "@heroicons/react/16/solid";
 
 function Product() {
   const params = useParams<{ id: string }>();
@@ -11,20 +12,34 @@ function Product() {
   useEffect(() => {
     getProduct(params.id as string).then((res) => {
       setProduct(res);
+      console.log(res);
     });
   }, []);
   return (
     <div className="bg-slate-100 px-10 py-20">
       <div className="grid grid-cols-2 gap-10">
-        <div className="col-span-1 p-10 rounded-2xl bg-white flex justify-center align-middle">
+        <div className="p-10 rounded-2xl bg-white flex justify-center align-middle min-h-120">
           <img className="w-2/3 object-contain" src={product?.image} />
         </div>
-        <div className="col-span-1 p-10 rounded-2xl bg-white">
-          <h3 className=" text-3xl text-slate-600 mb-10">{product?.title}</h3>
-          <p className="text-gray-600 text-base/7">{product?.description}</p>
-          <span className="text-2xl text-green-700 font-semibold mt-8 block">
-            ${product?.price}
-          </span>
+        <div className="p-10 rounded-2xl bg-white flex flex-col justify-between">
+          <div className="">
+            <h3 className="text-2xl font-semibold text-slate-600 mb-10">{product?.title}</h3>
+            <p className="text-gray-600 text-base/7 mb-4">{product?.description}</p>
+            <div className="flex gap-1 mb-5">
+              <StarIcon className="size-5 text-amber-400" />
+              <span className="text-base text-slate-700 font-semibold">
+                {product?.rating.rate}
+              </span>
+            </div>
+            <div className="text-2xl text-slate-600 font-semibold">
+              ${product?.price}
+            </div>
+          </div>
+          <div className="mt-10">
+            <button className="bg-green-700 text-white w-full py-2 rounded-lg">
+              + Add To Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
