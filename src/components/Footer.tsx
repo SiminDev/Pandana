1820a7824036d61f58a4af6284eab5ed2e039955
SiminDev <simin.rahmani94@gmail.com>
2025-02-26 23:32:45 +0330

    Edit footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,37 @@
 function Footer() {
   return (
-    <footer className="bg-black px-10 py-10">
-      <div className="grid grid-cols-8 gap-3 text-white">
-        <div className="col-span-5 flex flex-col gap-1.5">
-          <img src="/assets/logo-white.svg" className="w-40" />
-          <p className="font-light">Discover style and Innovation Together</p>
+    <footer className="bg-gray-100 px-10 py-15">
+      <div className="grid grid-cols-6 gap-3">
+        <div className="col-span-3 flex flex-col">
+          <img src="/assets/logo-black.svg" className="w-40 mb-4" />
+          <p className="text-gray-500 text-sm mb-1.5">© 2025 Pandana</p>
+          <p className="text-gray-500 text-sm">All Rights Reserved</p>
         </div>
-        <div className="col-span-1 flex- flex-col">
-          <ul className="text-sm font-light flex flex-col gap-1.5">
-            <li>Contact</li>
+        <div className="col-span-1 flex flex-col gap-5">
+          <span className="font-semibold">ABOUT US</span>
+          <ul className="text-sm text-gray-500 flex flex-col gap-3">
             <li>About us</li>
-            <li>FAQ</li>
-            <li>Privacy Policy</li>
+            <li>Store location</li>
+            <li>Contact</li>
+            <li>Orders tracking</li>
           </ul>
         </div>
-        <div className="col-span-1 flex- flex-col">
-          <ul className="text-sm font-light flex flex-col gap-1.5">
-            <li>info@pandana.com</li>
-            <li>+1 (416) 1123-4567</li>
+        <div className="col-span-1 flex flex-col gap-5">
+          <span className="font-semibold">USEFUL LINKS</span>
+          <ul className="text-sm text-gray-500 flex flex-col gap-3">
+            <li>Returns</li>
+            <li>Support Policy</li>
+            <li>Size guide</li>
+            <li>FAQs</li>
+          </ul>
+        </div>
+        <div className="col-span-1 flex flex-col gap-5">
+          <span className="font-semibold">FOLLOW US</span>
+          <ul className="text-sm text-gray-500 flex flex-col gap-3">
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+            <li>Youtube</li>
           </ul>
         </div>
       </div>
