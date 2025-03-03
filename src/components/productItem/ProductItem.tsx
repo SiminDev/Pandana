9822678156f6d2d6fa976885fdbdb72f1e39427c
SiminDev <simin.rahmani94@gmail.com>
2025-03-03 22:59:01 +0330

    impelent responsive HomePage

--- src/components/productItem/ProductItem.tsx
+++ src/components/productItem/ProductItem.tsx
@@ -4,21 +4,25 @@ import { Link } from "react-router-dom";
 
 function ProductItem({ title, image, price, rating, id }: Product) {
   return (
-    <Link to={`/product/${id}`}>
-      <div className="bg-white p-5 shadow-xl shadow-gray-100 rounded-2xl border border-slate-100">
-        <div className=" rounded-2xl flex justify-center overflow-hidden">
-          <img className="h-72 object-contain scale-85 hover:scale-100 transition duration-300" src={image} />
+    <Link
+      to={`/product/${id}`}
+      className="w-1/2 lg:w-full flex-shrink-0 bg-white p-5 shadow-xl shadow-gray-100 rounded-2xl border border-slate-100"
+    >
+      <div className="rounded-2xl flex justify-center overflow-hidden">
+        <img
+          className="h-48 md:h-72 object-contain scale-85 hover:scale-100 transition duration-300"
+          src={image}
+        />
+      </div>
+      <div className="mt-3 flex flex-col gap-1.5">
+        <h3 className="text-sm font-medium text-gray-800 line-clamp-1">
+          {title}
+        </h3>
+        <div className="flex gap-1">
+          <StarIcon className="size-5 text-amber-400" />
+          <span className="text-sm text-gray-700">{rating.rate}</span>
         </div>
-        <div className="mt-3 flex flex-col gap-1.5">
-          <h3 className="text-sm font-medium text-gray-800 line-clamp-1">
-            {title}
-          </h3>
-          <div className="flex gap-1">
-            <StarIcon className="size-5 text-amber-400" />
-            <span className="text-sm text-gray-700">{rating.rate}</span>
-          </div>
-          <p className="text-gray-500 font-semibold">${price}</p>
-        </div>
+        <p className="text-gray-500 font-semibold">${price}</p>
       </div>
     </Link>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,16 +13,17 @@ function Home() {
   });
   return (
     <>
-      <div className="flex px-15 py-10 bg-slate-100">
+      <div className="flex px-5 py-5 lg:px-15 lg:py-10 bg-slate-100">
         <Slider />
       </div>
-      <section className="px-15 py-20">
-        <h2 className="text-2xl mb-12 font-light text-gray-600 tracking-wide">
+      <section className="px-5 py-10 lg:px-15 lg:py-20">
+        <h2 className="text-lg lg:text-2xl mb-8 md:mb-12 font-light text-gray-600 tracking-wide">
           Newest Arrivals
         </h2>
-        <div className="grid grid-cols-4 gap-10">
+
+        <div className="flex flex-nowrap lg:flex-wrap lg:grid lg:grid-cols-4 gap-4 lg:gap-10 overflow-x-auto whitespace-nowrap">
           {products.map((item) => (
-              <ProductItem key={item.id} {...item} />
+            <ProductItem key={item.id} {...item} />
           ))}
         </div>
       </section>
