64c298e8ae573a9f3b68de9bdf84b70781d74190
SiminDev <simin.rahmani94@gmail.com>
2025-03-05 16:39:13 +0330

    implement responsive login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -15,10 +15,10 @@ function Login() {
   const { handleLogin } = useLoginContext();
 
   return (
-    <div className="flex justify-center py-20">
-      <div className="bg-slate-100 rounded-2xl w-2/5 flex flex-col justify-center items-center gap-3.5 p-10">
+    <div className="flex justify-center p-10 lg:py-20 min-h-120 lg:min-h-0">
+      <div className="bg-slate-100 rounded-2xl w-full md:w-2/3 lg:w-2/5 flex flex-col justify-center items-center gap-3.5 p-5 lg:p-10">
         <input
-          className="bg-white px-4 py-2 w-3/4 rounded-lg"
+          className="bg-white px-4 py-2 w-full lg:w-3/4 rounded-lg"
           type="text"
           name="username"
           value={user.username}
@@ -26,7 +26,7 @@ function Login() {
           onChange={handleChange}
         ></input>
         <input
-          className="bg-white px-4 py-2 w-3/4 rounded-lg"
+          className="bg-white px-4 py-2 w-full lg:w-3/4 rounded-lg"
           type="password"
           name="password"
           value={user.password}
@@ -35,7 +35,7 @@ function Login() {
         ></input>
 
         <button
-          className="w-3/4 rounded-lg bg-green-700 text-white py-2 cursor-pointer mt-5"
+          className="w-full lg:w-3/4 rounded-lg bg-green-700 text-white py-2 cursor-pointer mt-5"
           onClick={() => handleLogin(user.username, user.password)}
         >
           Login
